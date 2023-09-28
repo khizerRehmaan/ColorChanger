18603df55fc6508d893df3d9136141552292a590
khizerRehmaan <khizerrehman201819@gmail.com>
2023-09-28 11:49:08 +0500

    initial

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector(".box1").addEventListener("click", () => {
+  document.querySelector("body").style.backgroundColor = "aqua";
+});
+document.querySelector(".box2").addEventListener("click", () => {
+  document.querySelector("body").style.backgroundColor = "red";
+});
+document.querySelector(".box3").addEventListener("click", () => {
+  document.querySelector("body").style.backgroundColor = "yellow";
+});
+document.querySelector(".box4").addEventListener("click", () => {
+  document.querySelector("body").style.backgroundColor = "green";
+});
